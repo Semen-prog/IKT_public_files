6f4377ccded8a57c935f2fc550d5fb2d58b89f05
Semen-prog <82077476+Semen-prog@users.noreply.github.com>
2022-02-10 21:42:57 +0300

    Добавлена функция построения графиков

--- BATTLES_CSV/main.py
+++ BATTLES_CSV/main.py
@@ -1,6 +1,8 @@
 import database_process as db
 from tkinter import ttk
 from tkinter import *
+
+from painting import paint_pie, paint_date
 from user import User
 
 W, H = 500, 500
@@ -8,6 +10,7 @@ master = Tk()
 master.geometry('500x500')
 
 bat_size = 660
+
 
 def find():
     s = cmb1.get()
@@ -67,7 +70,8 @@ def find():
     elif s == "Weather":
         l1 = Label(m, text="Осадки:", height=5, width=20, font="Arial 10")
         l2 = Label(m, text="Температура:", height=5, width=20, font="Arial 10")
-        c1 = ttk.Combobox(m, values=["Heavy Precipitatiion", "Sunny (no precipitation)", "Light Precipitation", "Overcast (no precipitation)"])
+        c1 = ttk.Combobox(m, values=["Heavy Precipitatiion", "Sunny (no precipitation)", "Light Precipitation",
+                                     "Overcast (no precipitation)"])
         c2 = ttk.Combobox(m, values=["Hot", "Cold", "Temperate"])
         but1 = Button(m, text="Сохранить", bg="white", command=lambda: user.update([[c1.get(), 5], [c2.get(), 6]]))
         but2 = Button(m, text="Удалить выбор", bg="white", command=lambda: user.update([[None, 5], [None, 6]]))
@@ -93,16 +97,138 @@ def find():
 
     mainloop()
 
-def paint():
-    pass
+
+def members_func(res):
+    conn, cur = db.get_connection()
+    ask = """SELECT attacker, defender FROM battle_dyads
+        INNER JOIN battles
+            ON battle_dyads.isqno = battles.isqno
+        WHERE """
+    for r in res:
+        ask += "battles.isqno = " + str(r) + " OR "
+    ask = ask[:-4]
+    cur.execute(ask)
+    tab = cur.fetchall()
+    attackers, defenders = {}, {}
+    for [attacker, defender] in tab:
+        if attacker not in attackers:
+            attackers[attacker] = 0
+        attackers[attacker] += 1
+        if defender not in defenders:
+            defenders[defender] = 0
+        defenders[defender] += 1
+    m = Tk()
+    m.geometry('500x500')
+    l1 = Label(m, text="Построить график по:", height=5, width=20, font="Arial 10")
+    b1 = Button(m, text="Атакующим", bg="white", command=lambda: paint_pie(attackers))
+    b2 = Button(m, text="Защищающимся", bg="white", command=lambda: paint_pie(defenders))
+    l1.grid(row=0, column=1)
+    b1.grid(row=2, column=1)
+    b2.grid(row=4, column=1)
+
+    m.mainloop()
+
+
+def weather_func(res):
+    conn, cur = db.get_connection()
+    ask = """SELECT description FROM weather
+        INNER JOIN enum_wx2
+            ON weather.wx2 = enum_wx2.value
+        WHERE """
+    for r in res:
+        ask += "isqno = " + str(r) + " OR "
+    ask = ask[:-4]
+    cur.execute(ask)
+    tab = cur.fetchall()
+    precipitation, temperature = {}, {}
+    for [precip] in tab:
+        if precip not in precipitation:
+            precipitation[precip] = 0
+        precipitation[precip] += 1
+    ask = """SELECT description FROM weather
+            INNER JOIN enum_wx3
+                ON weather.wx3 = enum_wx3.value
+            WHERE """
+    for r in res:
+        ask += "isqno = " + str(r) + " OR "
+    ask = ask[:-4]
+    cur.execute(ask)
+    tab = cur.fetchall()
+    for [temp] in tab:
+        if temp not in temperature:
+            temperature[temp] = 0
+        temperature[temp] += 1
+    m = Tk()
+    m.geometry('500x500')
+    l1 = Label(m, text="Построить график по:", height=5, width=20, font="Arial 10")
+    b1 = Button(m, text="Облачности", bg="white", command=lambda: paint_pie(precipitation))
+    b2 = Button(m, text="Температуре", bg="white", command=lambda: paint_pie(temperature))
+    l1.grid(row=0, column=1)
+    b1.grid(row=2, column=1)
+    b2.grid(row=4, column=1)
+
+    m.mainloop()
+
+
+def time_func(res):
+    conn, cur = db.get_connection()
+    ask = """SELECT datetime_min, duration1 FROM battle_durations
+            INNER JOIN battles
+                ON battle_durations.isqno = battles.isqno
+            WHERE """
+    for r in res:
+        ask += "battles.isqno = " + str(r) + " OR "
+    ask = ask[:-4]
+    cur.execute(ask)
+    tab = cur.fetchall()
+    times_start, durations = {}, {}
+    for [tm, dr] in tab:
+        tm, dr = int(tm[:4]), float(dr)
+        if tm not in times_start:
+            times_start[tm] = 0
+        times_start[tm] += 1
+        if dr not in durations:
+            durations[dr] = 0
+        durations[dr] += 1
+    m = Tk()
+    m.geometry('500x500')
+    l1 = Label(m, text="Построить график по:", height=5, width=20, font="Arial 10")
+    b1 = Button(m, text="Году начала", bg="white", command=lambda: paint_date(times_start))
+    b2 = Button(m, text="Длительности", bg="white", command=lambda: paint_date(durations))
+    l1.grid(row=0, column=1)
+    b1.grid(row=2, column=1)
+    b2.grid(row=4, column=1)
+
+    m.mainloop()
+
+
+def to_paint(tab, s):
+    if s == '':
+        return
+    if s == 'Members':
+        members_func(tab)
+    elif s == 'Weather':
+        weather_func(tab)
+    else:
+        time_func(tab)
+
+
+def paint(res):
+    m = Tk()
+    m.geometry("500x500")
+    l1 = Label(m, text="Построить график по:", height=5, width=20, font="Arial 10")
+    cmb1 = ttk.Combobox(m, values=['Weather', 'Members', 'Time'])
+    b1 = Button(m, text="Построить", bg="white", command=lambda: to_paint(res, cmb1.get()))
+    l1.grid(row=0, column=1)
+    cmb1.grid(row=1, column=1)
+    b1.grid(row=2, column=1)
+
+    m.mainloop()
+
 
 def search():
-    m = Tk()
-    m.geometry('1000x1000')
     isqno = set([i for i in range(1, bat_size + 1)])
     conn, cur = db.get_connection()
-    # scrollbar = Scrollbar(m)
-    # scrollbar.pack(side="right", fill="y")
     if user.name:
         ask = 'SELECT isqno FROM battles WHERE name = "' + user.name + '"'
         cur.execute(ask)
@@ -173,45 +299,18 @@ def search():
         for v in vars:
             st.add(v[0])
         isqno &= st
-    if len(isqno) == 0:
-        lab = Label(m, text="Нет результатов", height=2, width=45, font="Arial 10 bold")
-        lab.grid(row=0, column=0)
-    else:
-        ask = """SELECT name, war FROM battles
-        WHERE """
-        for el in isqno:
-            cur.execute("SELECT name FROM battles WHERE isqno = " + str(el))
-            a = cur.fetchone()[0]
-            ask += "name = '" + a + "' OR "
-        ask = ask[:-4]
-        cur.execute(ask)
-        tab = cur.fetchall()
-        lab = Label(m, text="Результаты поиска:", height=2, width=15, font="Arial 10")
-        lab.grid(row=0, column=0)
-        trans = ['NAME', 'WAR']
-        for j in range(len(tab[0])):
-            lab = Label(m, text=trans[j], height=3, font="Arial 10 bold")
-            lab.grid(row=1, column=j)
-            for i in range(len(tab)):
-                lab = Label(m, text=tab[i][j], height=3, font="Arial 10")
-                lab.grid(row=i+2, column=j)
-    mainloop()
+    paint(list(isqno))
+
 
 user = User()
 
-l2 = Label(master, text="Инфографика:", height=8, width=20, font="Arial 10")
 l1 = Label(master, text="Фильтры битвы:", height=5, width=20, font="Arial 10")
 cmb1 = ttk.Combobox(master, values=['Name', 'Weather', 'Members', 'Time'])
-cmb2 = ttk.Combobox(master, values=['Date', 'Precipitation', "Temperature", 'Members', 'Duration'])
 b1 = Button(master, text="Выбрать параметры", bg="white", command=find)
-b2 = Button(master, text="Построить график", bg="white", command=paint)
-b3 = Button(master, text="Найти битву", bg="white", command=search)
-l2.grid(row=3, column=0)
+b2 = Button(master, text="Найти битву", bg="white", command=search)
 l1.grid(row=0, column=0)
 cmb1.grid(row=0, column=1)
-cmb2.grid(row=3, column=1)
 b1.grid(row=1, column=1)
-b2.grid(row=4, column=1)
-b3.grid(row=2, column=1)
+b2.grid(row=2, column=1)
 
 mainloop()
